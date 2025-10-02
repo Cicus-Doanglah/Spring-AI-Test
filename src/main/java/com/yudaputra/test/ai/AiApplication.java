23d8f914ed67f7cb663021a65130515c2bea2fd5
david <david.yudaputra@gmail.com>
2025-10-02 14:54:47 +0700

    disable any shit

--- src/main/java/com/yudaputra/test/ai/AiApplication.java
+++ src/main/java/com/yudaputra/test/ai/AiApplication.java
@@ -1,9 +1,16 @@
 package com.yudaputra.test.ai;
 
+import org.springframework.ai.model.openai.autoconfigure.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		OpenAiAudioSpeechAutoConfiguration.class,
+		OpenAiEmbeddingAutoConfiguration.class,
+		OpenAiImageAutoConfiguration.class,
+		OpenAiAudioTranscriptionAutoConfiguration.class,
+		OpenAiModerationAutoConfiguration.class
+})
 public class AiApplication {
 
 	public static void main(String[] args) {
